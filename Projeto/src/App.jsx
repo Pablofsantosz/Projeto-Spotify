de8910b713ccaa28dd5d329b61d73de687d0bfeb
pablo felipe <pabloletiane@gmail.com>
2025-05-06 18:14:00 -0300

    header

--- Projeto/src/App.jsx
+++ Projeto/src/App.jsx
@@ -1,11 +1,11 @@
+//rafce(comando) que cria uma funtion arrow == react arrow function componet export 
 
+import Header from "./components/Header"
+
+//Maneira rapida de criar nosso componetes
 function App() {
   
-  return (
-    <>
-     
-    </>
-  )
+  return <Header/>;
 }
 
 export default App
