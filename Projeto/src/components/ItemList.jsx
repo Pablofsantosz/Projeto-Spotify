d3f2badb2dac8ecbaaadca2cb8b21d70834b3cde
pablo felipe <pabloletiane@gmail.com>
2025-05-11 11:13:58 -0300

    commit

--- Projeto/src/components/ItemList.jsx
+++ Projeto/src/components/ItemList.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import SingleItem from "./SingleItem";
-import { Link } from "react-router-dom";
+import { Link , useLocation} from "react-router-dom";
 
 const ItemList = ({ title, items, itemsArray, path, idPath }) => {
- 
 
+  const { pathname } = useLocation();
+  const isInHome =pathname === "/";
+
+  const finalItens = isInHome ? items : Infinity
   return (
     <div className="item-list">
       <div className="item-list__header">
         <h2>{title} populares</h2>
-        <Link to={path} className="item-list__link">
-          Mostrar tudo
-        </Link>
+        {isInHome ? (
+          <Link to={path} className="item-list__link">
+            Mostrar tudo
+          </Link> ): (
+          <></>)
+        }
+        
       </div>
 
       <div className="item-list__container">
         {itemsArray
-          .filter((currentValue, index) => index < items)
+          .filter((currentValue, index) => index < finalItens)
           .map((currObj, index) => (
             <SingleItem
               // id={currObj.id}
